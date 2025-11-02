86982fb3cb580242074ea7933195e23e59ad4b44
Arun <arunramkrishna997@gmail.com>
2025-11-02 18:43:18 -0500

    check

--- CsharpWebIDE/obj/Debug/net9.0/CsharpWebIDE.AssemblyInfo.cs
+++ CsharpWebIDE/obj/Debug/net9.0/CsharpWebIDE.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CsharpWebIDE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9965394b343fc55246e57b8914eb29b7b05585f")]
 [assembly: System.Reflection.AssemblyProductAttribute("CsharpWebIDE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CsharpWebIDE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
